bf16f87c3078b89a7c537c1f4b9c5b4eece1d25f
sarahfad <you@example.com>
2025-09-09 03:47:45 +0000

    tasks 1 and 2

---
+++ homework1/src/task1.py
@@ -0,0 +1,4 @@
+def greeting():
+    return print("Hello, World!")
+
+greeting()
---
+++ homework1/src/task2.py
@@ -0,0 +1,18 @@
+def integer_num():
+    return 20
+
+def float_num():
+    return 2.12
+
+def strings():
+    return "Hello, World!"
+
+def boolean():
+    return True
+
+integer_num = integer_num()
+float_num = float_num()
+strings = strings()
+boolean = boolean()
+
+print(f"integer: {integer_num}, float: {float_num}, string: {strings}, boolean: {boolean}")
